522067c29e53f309d6ef9c51e0d599da459d48c9
edytamatuszczak <edyta.matuszczak@elev.ga.ntig.se>
2024-03-25 10:20:54 +0100

    Gjorde en spel window

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+            JFrame f = new JFrame("Pacman");
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            f.setSize(600, 600 );
+            f.setVisible(true);
+
+            f.repaint();
     }
 }
